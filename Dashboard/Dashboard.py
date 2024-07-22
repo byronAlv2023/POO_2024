d411796c3a21ea100a0857916063460a4470eb26
Byron Alvarado <be.alvaradog@uea.edu.ec>
2024-07-21 20:46:04 -0500

    Tarea semana 8

---
+++ Dashboard/Dashboard.py
@@ -0,0 +1,49 @@
+import os
+
+
+def mostrar_codigo(ruta_script):
+    # Asegúrate de que la ruta al script es absoluta
+    ruta_script_absoluta = os.path.abspath(ruta_script)
+    try:
+        with open(ruta_script_absoluta, 'r') as archivo:
+            print(f"\n--- Código de {ruta_script} ---\n")
+            print(archivo.read())
+    except FileNotFoundError:
+        print("El archivo no se encontró.")
+    except Exception as e:
+        print(f"Ocurrió un error al leer el archivo: {e}")
+
+
+def mostrar_menu():
+    # Define la ruta base donde se encuentra el dashboard.py
+    ruta_base = os.path.dirname(__file__)
+
+    opciones = {
+        '1': 'Clases, objetos, herencia, encapsulamiento y polimorfismo/aplicacion.de_conceptos.py',
+        '2': 'Constructores_y_Destructores/init_y_Del.py',
+        '3': 'EjemplosMundoReal_POO/Sistema_de_Gestión_de_Empleados.py/Sistema_de_Gestión_de_Estudiantes.py/Sistema_de_Gestión_de_Libros.py',
+        '4': 'Tipos de datos_identificadores/Identificadores.py',
+        # Agrega aquí el resto de las rutas de los scripts
+ }
+
+    while True:
+        print("\nMenu Principal - Dashboard")
+        # Imprime las opciones del menú
+        for key in opciones:
+            print(f"{key} - {opciones[key]}")
+        print("0 - Salir")
+
+        eleccion = input("Elige un script para ver su código o '0' para salir: ")
+        if eleccion == '0':
+            break
+        elif eleccion in opciones:
+            # Asegura que el path sea absoluto
+            ruta_script = os.path.join(ruta_base, opciones[eleccion])
+            mostrar_codigo(ruta_script)
+        else:
+            print("Opción no válida. Por favor, intenta de nuevo.")
+
+
+# Ejecutar el dashboard
+if __name__ == "__main__":
+    mostrar_menu()
